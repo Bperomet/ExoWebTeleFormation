021dca5bd575d6abbf9f75662548d12a60700f19
Bperomet <43849967+Bperomet@users.noreply.github.com>
2020-04-16 09:40:23 +0200

    2.0.0

--- FilRouge/Authentificateur/Authentificateur.js
+++ FilRouge/Authentificateur/Authentificateur.js
@@ -1,71 +1,27 @@
-
-
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('AuthDB.db');
+const sqlModule = require('./gestionSql');
 
 function User(id,surname,lastname,email,password,descrip,role){
-    this.idUser = id||'';
-    this.surnameUser = surname||'';
-    this.lastnameUser = lastname||'';
-    this.emailUser = email||'';
-    this.passwordUser = password||'';
-    this.descriptionUser = descrip||'';
-    this.roleUser = role||'Usager';
+  this.idUser = id||'';
+  this.surnameUser = surname||'';
+  this.lastnameUser = lastname||'';
+  this.emailUser = email||'';
+  this.passwordUser = password||'';
+  this.descriptionUser = descrip||'';
+  this.roleUser = role||'Usager';
 }
 
-let user;
-
-db.serialize(function(){
-
-    db.run('CREATE TABLE IF NOT EXISTS userData (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, email TEXT, password TEXT, description TEXT, role TEXT)');
-
-    db.all('SELECT ID, FIRSTNAME, LASTNAME, EMAIL, PASSWORD, DESCRIPTION, ROLE FROM userData', function (err, row) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(row.length === 0){
-                var stmt = db.prepare('INSERT INTO userData (firstname, lastname, email, password, description, role) VALUES (?, ?, ?, ?, ?, ?)');
-                var obj = [{ firstname:'FirstName', lastname:'LastName', email:'email@email.ml', 
-                    password:'Password', description:'Description', role:'Usager'}];
-                for(let i in obj){
-
-                    stmt.run(obj[i].firstname, obj[i].lastname, obj[i].email, obj[i].password, obj[i].description, obj[i].role);
-                }
-                stmt.finalize();
-            }
-            else{
-                console.log('Base de donnée éxistante.');
-            }
-
-        }
-    });
-});
+const InitialiseDB = ()=>{
+  sqlModule.CreatDB();
+}
 
 const GetData = (app)=>{
-app.get('/database', function (req, res) {
-    db.all('SELECT ID, FIRSTNAME, LASTNAME, EMAIL, PASSWORD, DESCRIPTION, ROLE FROM userData', function (err, rows) {
-        var output = [];
-        if (err) {
-          console.log(err);
-        } 
-        else {
-          if (rows.length === 0) {
-            res.send('Empty database');
-          } 
-          else {
-            rows.forEach(function (row) {
-              output.push({ id: row.id, firstname: row.firstname, lastname: row.lastname, 
-                email: row.email, password: row.password, 
-                description: row.description, role: row.role});
-            });
-            res.send(output);
-          }
-        }
-      });
+  app.get('/database', function (req, res) {
+    sqlModule.getAll(function(users){
+      res.send(users);
+    });
  });
-}
+};
+
 const AddData = (app)=>{
 app.post('/add', function (req, res) {
    var FirstNameValue = req.body.firstname;
@@ -75,97 +31,103 @@ app.post('/add', function (req, res) {
    var DescriptionValue = req.body.description;
    var RoleValue = req.body.role;
 
-  db.each('SELECT EMAIL FROM userData WHERE email=? UNION ALL SELECT NULL LIMIT 1', EmailValue, function (err, row) {
-    if (err) {
-        console.log(err);
-    }
-    if (row.email === null) {
-      db.run('INSERT INTO userData (firstname, lastname, email, password, description, role) VALUES (?, ?, ?, ?, ?, ?)', 
-      FirstNameValue, LastNameValue, EmailValue, PasswordValue, DescriptionValue, RoleValue, function (err, row) {
-        if (err) {
-            console.log(err);
-        } 
-        else {
-            res.send('Success');
-        }
-      });
+  sqlModule.add(FirstNameValue, LastNameValue, EmailValue, PasswordValue, DescriptionValue, RoleValue, function(callback){
+    if (callback !== null) {
+      console.log(callback);
     }
     else{
-      res.send('Email deja existant')
+      console.log('La creation a echoue');
     }
   });
  });
-}
+};
+
 const TryConect =(app)=>{
  app.post('/connection', function (req, res) {
     var EmailValue = req.body.email;
     var PasswordValue = req.body.password;
- 
-     db.each('SELECT ID FROM userData WHERE email=? AND password=? UNION ALL SELECT NULL LIMIT 1', EmailValue, PasswordValue, function (err, row) {
-     if (err) {
-         console.log(err);
-     }
-     //si ça matche
-     if (row.id != null) {
-        user = new User(row.id,row.surname,row.lastname,row.email,row.password,row.descrip,row.role);
 
-     //   app.set('view engine','ejs');
-        if(user.roleUser==='administrateur'){
-         // res.render('/AdminPage');
-        //  res.send('Vous etes dans la zone de gestion faite pour les administrateur!');
-          
-        }
-        else{
-         // res.render('index.html');
+    sqlModule.get(EmailValue,PasswordValue, function(user){
+      console.log(user);
+      if(user != null){
+        //console.log(user);
+//Test Update
+        user ={
+          id: user.id,
+          firstname: user.firstname,
+          lastname: "bambam",
+          email: "email@new.ml",
+          password: user.password,
+          description: user.description,
+          role: "Admine", 
+        };
 
-         // res.send('Vous etes dans la zone faite pour les utilisateur!');
-        }
-     } 
-     else {
-         res.send('Identifiants non existant');
-     }
-     });
- 
+        sqlModule.update(user,function(callback){
+          if (callback !== null) {
+            console.log('User modifier');
+          }
+          else{
+            console.log(callback +" pas update");
+          }
+        });
+      }
+    });
   });
-}
+};
+
 const BonusDelete =(app)=>{
   app.post('/delete', function (req, res) {
     var IdValue = req.body.id;
-    //verrif
-    if (IdValue !== '' && IdValue !== undefined) {
-        //verrif si l'id match
-      db.each('SELECT ID FROM userData WHERE id=? UNION ALL SELECT NULL LIMIT 1', IdValue, function (err, row) {
-        if (err) {
-          console.log(err);
-        }
-        if (row.id === null) {
-          res.send('You should specify an ID');
-        } 
-        else {
-            //lance la requete
-          db.run('DELETE FROM userData WHERE id=?', IdValue, function (err) {
-            if (err) {
-              console.log(err);
-            } 
-            else {
-              res.send('Success');
-            }
-          })
-        }
-      });
-    } 
-    else {
-      res.send('Unable to delete data. Check syntax');
-    }
+    sqlModule.remove(IdValue,function(callback){
+      if (callback) {
+        console.log('User supprimer');
+      }
+      else{
+        console.log('suppression impossible');
+      }
+    });
   });
-}
+};
+
+const BonusUpdate =(app)=>{
+  app.post('/update', function (req, res) {
+    var IdValue = req.body.id;
+    
+    sqlModule.update(user,function(callback){
+      if (callback != null) {
+        console.log('User modifier');
+      }
+      else{
+        console.log('impossible de faire la modification');
+      }
+    });
+  });
+};
+
   module.exports = {
     get:GetData,
     add:AddData,
     connexion: TryConect,
-    delete: BonusDelete
+    delete: BonusDelete,
+    initialiseDB: InitialiseDB,
+    update: BonusUpdate
   };
 /*
 curl -d "{\"firstname\" : \"Lewis\",\"lastname\" : \"Carroll\",\"email\" : \"test@email.fr\",\"password\" : \"azertyx\",\"description\" : \"je suis une description\",\"role\" : \"Usager\"}" -H "Content-Type: application/json" -X POST "http://localhost:9500/add"
-curl -d "{\"email\" : \"test@email.fr\",\"password\" : \"azertyx\"}" -H "Content-Type: application/json" -X POST "http://localhost:9500/connection"
+curl -d "{\"email\" : \"email@new.ml\",\"password\" : \"azertyx\"}" -H "Content-Type: application/json" -X POST "http://localhost:9500/connection"
 */
+
+    
+     //   app.set('view engine','ejs');
+    // user =null;
+   //  console.log(user)
+   /* if(user !== null){
+      if(user.role==='administrateur'){
+        // res.render('/AdminPage');
+       //  res.send('Vous etes dans la zone de gestion faite pour les administrateur!');
+       }
+       else{
+        // res.render('index.html');
+        // res.send('Vous etes dans la zone faite pour les utilisateur!');
+       }
+     }*/
--- FilRouge/Authentificateur/testModule.js
+++ FilRouge/Authentificateur/testModule.js
@@ -3,10 +3,12 @@ const app = express();
 app.use(express.json());
 const importModule = require('./Authentificateur');
 //const importModule = require('./Authentificateur')(app);
-
+importModule.initialiseDB();
 importModule.get(app);
 importModule.connexion(app);
 importModule.add(app);
+importModule.delete(app);
+
 
 app.set('port',9500);
 console.log('Le serveur ecoute sur le port ',app.get('port'));
